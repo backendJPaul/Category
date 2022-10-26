594d58a047c4017702a2b225b0c8662863ddd22a
backendJPaul <files.pticlavilca@gmail.com>
2022-10-26 07:11:12 -0500

    add DAOmanager

--- src/main/java/com/jpaul/App.java
+++ src/main/java/com/jpaul/App.java
@@ -1,27 +1,11 @@
 package com.jpaul;
 
-import com.jpaul.dao.CategoryDAO;
-import com.jpaul.dao.DAO;
-import com.jpaul.model.Category;
-
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import com.jpaul.dao.impl.DAOManager;
 
 public class App{
     public static void main(String args[]) throws Exception {
-        CategoryDAO categoryDAO = new CategoryDAO();
 
-        Category category = new Category();
-        category.setIdCategory(8);
-
-
-        Category category1 = categoryDAO.gotoId(category);
-
-        System.out.println(category1.getIdCategory());
-        System.out.println(category1.getName());
+        DAOManager daoManager = new DAOManager();
+        System.out.println("read() " + daoManager.getCategoryDAO().read().toString());
     }
 }
